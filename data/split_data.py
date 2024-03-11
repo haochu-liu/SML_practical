88eb6e471df78b11811cdaecbb24576461e04942
haochu-liu <Liu_haochu@outlook.com>
2024-03-11 12:16:36 +0000

    PCA

--- data/split_data.py
+++ data/split_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import sklearn
 print('The scikit-learn version is {}.'.format(sklearn.__version__))
 
@@ -19,6 +21,15 @@ X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=sp
 scalerx = StandardScaler()
 X_train = scalerx.fit_transform(X_train)
 X_val = scalerx.transform(X_val)
+
+# PCA to reduce dimensions
+p_PCA = 25 # from notebook pictures
+pca = PCA(n_components=p_PCA)
+pca.fit(X_train_sc)
+Z_pca = pca.transform(X_train_sc)
+X_train_PCA = pd.DataFrame(Z_pca[:, [0, 1, 2]], columns=['PC1', 'PC2', 'PC3'])
+plt.figure()
+sns.pairplot(data=X_train_PCA)
 
 # Save the scaled data sets
 np.savetxt('data/X_train_sc.csv', X_train, delimiter=',')
